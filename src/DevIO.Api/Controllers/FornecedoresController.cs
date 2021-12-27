9cc0a7eb9fd6628a985e4aa10d557225ab695ae8
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2021-12-27 17:21:56 -0300

    Inicio + implementação de DTOs (ViewModels)

---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using DevIO.Business.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IFornecedorService _fornecedorService;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+            var fornecedor = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+            return fornecedor;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
+        {
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            return fornecedor;
+        }
+
+        public async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
+        }
+
+    }
+}
---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // Validação de notificãções de erro
+        
+        // Validação de modelstate (preenchimento correto das viewmodels)
+
+        // Validação da operação de negócios
+
+        //
+    }
+}
